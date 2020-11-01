9d54c5673a41483c9879c0edb9313f2bc63dc9f4
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-11-01 12:23:18 +0000

    feat: code can now process vector of json objects

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::collections::HashMap;
+use serde::Deserialize;
+use reqwest::Error;
+
+#[derive(Deserialize, Debug)]
+struct User {
+    userId: u32,
+    id: u32,
+    title: String
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let response = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", response);
+async fn main() -> Result<(), Error> {
+    let request_url = String::from("https://jsonplaceholder.typicode.com/posts");
+    println!("{}", request_url);
+    let response = reqwest::get(&request_url).await?;
+
+    let users: Vec<User> = response.json().await?;
+    println!("{:?}", users);
     Ok(())
 }
