37095217031eba4fd1f7d6a927ff934e4f0ac640
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-11-10 20:03:51 +0000

    feat: added mongo client and made basic insert

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use std::error::Error;
 use time::now;
+use mongodb::{Client, bson};
 // use log;
 // use simple_logger::SimpleLogger;
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[allow(non_snake_case)]
 struct TimeSeriesCase {
     confirmed: i64,
@@ -28,7 +29,7 @@ struct Case {
     Active: i64,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[allow(non_snake_case)]
 struct CsvCase {
     Province_State: Option<String>,
@@ -68,6 +69,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let execution_start = now();
     // SimpleLogger::new().init().unwrap();
 
+    let client = Client::with_uri_str("mongodb://localhost:27017/").await?;
+    let db = client.database("covid19r");
+    let cases_collection = db.collection("casesByLocation");
+    for coll_name in db.list_collection_names(None).await? {
+        println!("collection: {}", coll_name);
+    }
+
     let gis_service = String::from("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query");
     let cases_by_country_query_params = String::from("?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=");
     let total_confirmed_cases_query_params = String::from("?where=(Confirmed+>+0)&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=[{'statisticType'%3A'sum'%2C'onStatisticField'%3A'Confirmed'%2C'outStatisticFieldName'%3A'value'}]&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=");
@@ -88,7 +96,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let deaths_global_cases = deaths_csv_response.text().await?;
 
     let processed_csv: Vec<CsvCase> = process_csv(confirmed_global_cases, deaths_global_cases)?;
-    println!("{:?}", processed_csv[0]);
+    println!("{:?} CSV cases... ", processed_csv.len());
+
+    let cases_bson = bson::to_document(&processed_csv[0]).unwrap();
+    let mut test_bson = Vec::new();
+    test_bson.push(cases_bson);
+    cases_collection.insert_many(test_bson, None).await?;
 
     let total_confirmed_url = format!("{}{}", gis_service, total_confirmed_cases_query_params);
     let total_confirmed_response = reqwest::get(&total_confirmed_url).await?;
