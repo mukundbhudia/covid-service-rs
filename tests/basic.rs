1c32533d2775d311ac6600a3520582f3ee9225da
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-11-28 12:34:56 +0000

    feat: added basic test

---
+++ tests/basic.rs
@@ -0,0 +1,13 @@
+pub fn my_challenge() -> bool {
+  true
+}
+
+#[cfg(test)]
+mod my_challenge_tests {
+  use super::*;
+  #[test]
+  fn general_tests() {
+      assert_eq!(my_challenge(), true);
+      assert_eq!(!my_challenge(), false);
+  }
+}
