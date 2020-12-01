12da3ad5a7b219001a8851dd22de2f8cb0b94542
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-12-01 20:26:32 +0000

    test: more for hyphenation

--- tests/data_processing_test.rs
+++ tests/data_processing_test.rs
@@ -9,6 +9,30 @@ fn test_string_hyphenation() {
 }
 
 #[test]
+fn test_empty_string_hyphenation() {
+    assert_eq!(
+        data_processing::hyphenate_string("".to_string()),
+        "".to_string()
+    );
+}
+
+#[test]
+fn test_multiple_worded_string_hyphenation() {
+    assert_eq!(
+        data_processing::hyphenate_string("Saint Vincent and the Grenadines".to_string()),
+        "saint-vincent-and-the-grenadines".to_string()
+    );
+}
+
+#[test]
+fn test_worded_start_and_end_with_space_string_hyphenation() {
+    assert_eq!(
+        data_processing::hyphenate_string(" San Marino ".to_string()),
+        "san-marino".to_string()
+    );
+}
+
+#[test]
 fn test_id_key_gen() {
     assert_eq!(
         data_processing::generate_id_key(&Some("Hong Kong".to_string()), &"China".to_string()),
