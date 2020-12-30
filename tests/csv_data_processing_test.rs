289f06a31eaa098731de10a5615293278623fc6f
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-12-30 20:16:33 +0000

    refactor: reordered and organised csv test

--- tests/csv_data_processing_test.rs
+++ tests/csv_data_processing_test.rs
@@ -4,7 +4,19 @@ use std::collections::{BTreeMap, HashMap};
 
 #[test]
 fn basic_process_one_country_4_days_global_csv() {
-    let mut afghanistan_time_series_cases = vec![
+    let global_confirmed_csv =
+        "Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20,1/25/20
+,Afghanistan,33.93911,67.709953,0,0,0,0"
+            .to_string();
+    let global_deaths_csv = "Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20,1/25/20
+,Afghanistan,33.93911,67.709953,0,0,0,0"
+        .to_string();
+
+    let global_confirmed_and_deaths_count = (0, 0);
+
+    let today = String::from("1/26/20");
+
+    let mut expected_afghanistan_time_series_cases = vec![
         TimeSeriesCase {
             confirmed: 0,
             deaths: 0,
@@ -43,48 +55,33 @@ fn basic_process_one_country_4_days_global_csv() {
             Country_Region: "Afghanistan".to_string(),
             Lat: 33.93911,
             Long_: 67.709953,
-            cases: afghanistan_time_series_cases.clone(),
+            cases: expected_afghanistan_time_series_cases.clone(),
         },
     );
 
     // We add the current day's cases to the expected time series
-    afghanistan_time_series_cases.push(TimeSeriesCase {
+    expected_afghanistan_time_series_cases.push(TimeSeriesCase {
         confirmed: 0,
         deaths: 0,
         confirmedCasesToday: 0,
         deathsToday: 0,
-        day: "1/26/20".to_string(),
+        day: today.clone(),
     });
 
     let first_day_csv_index = 4;
     let expected_time_series_map = (first_day_csv_index..)
-        .zip(afghanistan_time_series_cases)
+        .zip(expected_afghanistan_time_series_cases)
         .collect::<BTreeMap<usize, TimeSeriesCase>>();
-
-    let global_confirmed_csv =
-        "Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20,1/25/20
-,Afghanistan,33.93911,67.709953,0,0,0,0"
-            .to_string();
-    let global_deaths_csv = "Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20,1/25/20
-,Afghanistan,33.93911,67.709953,0,0,0,0"
-        .to_string();
-
-    let global_confirmed_and_deaths_count = (0, 0);
 
     let result = data_processing::process_csv(
         global_confirmed_csv,
         global_deaths_csv,
         Region::Global,
-        "1/26/20".to_string().to_string(),
+        today,
         Some(global_confirmed_and_deaths_count),
     );
 
-    let (actual_global_cases, actual_time_series_map) = match result {
-        Ok((actual_global_cases, actual_time_series_map)) => {
-            (actual_global_cases, actual_time_series_map)
-        }
-        Err(_) => panic!("Error extracting data from global time series."),
-    };
+    let (actual_global_cases, actual_time_series_map) = result.unwrap();
 
     assert_eq!(expected_global_cases, actual_global_cases);
     assert_eq!(expected_time_series_map, actual_time_series_map);
